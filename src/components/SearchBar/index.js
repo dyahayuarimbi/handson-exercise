47575a2ff1dc071eb90bad8ec33dd922b0528080
dyahayuarimbi <dyahayuarimbi@upi.edu>
2022-04-13 12:58:53 +0700

    use material-ui

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,8 +1,13 @@
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
 const SearchBar = ({ query, onChange, onSubmit }) => {
     return (
       <form onSubmit={onSubmit}>
-        <input type="text" name="query" value={query} onChange={onChange} />
-        <input type="submit" value="Search" />
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" name="query" value={query} onChange={onChange} />
+        <Button type="submit" variant="contained" color="success">
+          Search
+        </Button>
       </form>
     );
   };
